c86dfa485bff0cd5080ddbee7f3620616c296bbc
Naomi <naomi@example.com>
2025-08-06 14:30:07 +0900

    各セクションの結果、まとめを追記

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,11 +14,18 @@ export default function Home() {
         </h1>
 
         <h1
-          className="text-2xl font-bold mb-8 bg-gray-200 p-4 rounded"
+          className="text-2xl font-bold bg-gray-200 p-4"
           style={{ color: "#3f2c96" }}
         >
-          ▼ ① 親要素指定なし・子要素width={300}・height={300}
+          ① 親要素指定なし・子要素width={300}・height={300}
         </h1>
+        <p className="text-lg mb-8 bg-gray-200 p-4 ">
+          どの画像も、ブラウザで100%表示ではガビガビしている。
+          <br />
+          拡大すると、【600px】～がきれいに見える。
+          <br />
+          拡大しても、ある一定の大きさ以上にならない。
+        </p>
 
         <div className="grid grid-cols-3 gap-5 mb-10">
           <div>
@@ -138,11 +145,16 @@ export default function Home() {
       {/* *************親要素 w-[300px] h-[300px]・子要素 width={300}・height={300}************* */}
       <div className="max-w-6xl mx-auto p-5 mt-20">
         <h1
-          className="text-2xl font-bold mb-8 bg-gray-200 p-4 rounded"
+          className="text-2xl font-bold bg-gray-200 p-4"
           style={{ color: "#3f2c96" }}
         >
-          ▼ ② 親要素 w-[300px] h-[300px]・子要素 width={300}・height={300}
+          ② 親要素 w-[300px] h-[300px]・子要素 width={300}・height={300}
         </h1>
+        <p className="text-lg mb-8 bg-gray-200 p-4 ">
+          どの画像も、ブラウザで100%表示ではガビガビしている。①と同じ結果になる
+          <br />
+          拡大すると一定の大きさまではきれいだが、それ以上は全てガビっている。
+        </p>
 
         <div className="grid grid-cols-3 gap-5 mb-10">
           <div>
@@ -258,16 +270,20 @@ export default function Home() {
           </div>
         </div>
       </div>
-
 
       {/* *************親要素 w-[300px] h-[300px]・子要素 width={600}・height={600}************* */}
       <div className="max-w-6xl mx-auto p-5 mt-20">
         <h1
-          className="text-2xl font-bold mb-8 bg-gray-200 p-4 rounded"
+          className="text-2xl font-bold bg-gray-200 p-4"
           style={{ color: "#3f2c96" }}
         >
-          ▼ ③ 親要素 w-[300px] h-[300px]・子要素 width={600}・height={600}
+          ③ 親要素 w-[300px] h-[300px]・子要素 width={600}・height={600}
         </h1>
+        <p className="text-lg mb-8 bg-gray-200 p-4 ">
+          Obsidian 【600px】～【2000px】きれいに表示される。
+          <br />
+          拡大すると一定の大きさまではきれいだが、それ以上は全てガビっている。
+        </p>
 
         <div className="grid grid-cols-3 gap-5 mb-10">
           <div>
@@ -384,47 +400,39 @@ export default function Home() {
         </div>
       </div>
 
-
       {/* *************親要素 w-[300px] h-[300px]・子要素 fill************* */}
       <div className="max-w-6xl mx-auto p-5 mt-20">
         <h1
-          className="text-2xl font-bold mb-8 bg-gray-200 p-4 rounded"
+          className="text-2xl font-bold bg-gray-200 p-4"
           style={{ color: "#3f2c96" }}
         >
-          ▼ ④ 親要素 w-[300px] h-[300px]・子要素 fill
+          ④ 親要素 w-[300px] h-[300px]・子要素 fill
         </h1>
+        <p className="text-lg mb-8 bg-gray-200 p-4 ">
+          Obsidian 【600px】～【2000px】きれいに表示される。
+          <br />
+          拡大すればするほど、高解像度の画像が美しい
+        </p>
 
         <div className="grid grid-cols-3 gap-5 mb-10">
           <div>
             <h3>Obsidian 【50px】</h3>
             <div className="w-[300px] h-[300px] relative">
-              <Image 
-                src="/img/Obsidian_50px.png"
-                alt="Obsidian 50px" 
-                fill 
-              />
+              <Image src="/img/Obsidian_50px.png" alt="Obsidian 50px" fill />
             </div>
           </div>
 
           <div>
             <h3>Obsidian 【150px】</h3>
             <div className="w-[300px] h-[300px] relative">
-              <Image 
-                src="/img/Obsidian_150px.png" 
-                alt="Obsidian 150px" 
-                fill 
-              />
+              <Image src="/img/Obsidian_150px.png" alt="Obsidian 150px" fill />
             </div>
           </div>
 
           <div>
             <h3>Obsidian 【300px】</h3>
             <div className="w-[300px] h-[300px] relative">
-              <Image 
-                src="/img/Obsidian_300px.png" 
-                alt="Obsidian 300px" 
-                fill 
-              />
+              <Image src="/img/Obsidian_300px.png" alt="Obsidian 300px" fill />
             </div>
           </div>
         </div>
@@ -433,22 +441,14 @@ export default function Home() {
           <div>
             <h3>Obsidian 【500px】</h3>
             <div className="w-[300px] h-[300px] relative">
-              <Image 
-                src="/img/Obsidian_500px.png" 
-                alt="Obsidian 500px" 
-                fill 
-              />
+              <Image src="/img/Obsidian_500px.png" alt="Obsidian 500px" fill />
             </div>
           </div>
 
           <div>
             <h3>Obsidian 【600px】</h3>
             <div className="w-[300px] h-[300px] relative">
-              <Image 
-                src="/img/Obsidian_600px.png" 
-                alt="Obsidian 600px" 
-                fill 
-              />
+              <Image src="/img/Obsidian_600px.png" alt="Obsidian 600px" fill />
             </div>
           </div>
 
@@ -458,7 +458,7 @@ export default function Home() {
               <Image
                 src="/img/Obsidian_1000px.png"
                 alt="Obsidian 1000px"
-                fill 
+                fill
               />
             </div>
           </div>
@@ -499,6 +499,25 @@ export default function Home() {
           </div>
         </div>
       </div>
+
+      {/* *************まとめ************* */}
+      <div className="max-w-6xl mx-auto p-5 mt-20">
+        <h1
+          className="text-2xl font-bold bg-gray-200 p-4"
+          style={{ color: "#3f2c96" }}
+        >
+          まとめ
+        </h1>
+        <p className="text-lg mb-8 bg-gray-200 p-4 ">
+          親要素 w-[●●●px] h-[▲▲▲px]・子要素 fill の組み合わせで、
+          <br />
+          画像サイズは、親要素の２倍が美しいと感じた。
+        </p>
+      </div>
+
+      <footer className="text-center text-sm text-gray-500 mb-20">
+        <p>© 2025 松尾直実　｜　最終更新日: 2025-08-06</p>
+      </footer>
     </>
   );
 }
